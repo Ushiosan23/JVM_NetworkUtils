c5b25402d21a521c0f8c4e0379c18479706c3003
Ushiosan23 <haloleyendee@outlook.com>
2020-10-19 09:34:09 -0500

    feat(all): Initialize properties

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	id("java-library")
+	id("org.jetbrains.dokka") version "1.4.10"
+	id("maven-publish")
+	kotlin("jvm") version "1.4.10"
+	kotlin("plugin.serialization") version "1.4.10"
+}
+
+// Base configuration
+group = "com.github.ushiosan23.networkutils"
+version = "0.0.1"
+
+// Repositories
+repositories {
+	mavenCentral()
+	jcenter()
+}
+
+// Library Dependencies
+dependencies {
+	/* kotlin */
+	implementation(kotlin("stdlib"))
+	implementation(kotlin("reflect"))
+	/* serialization */
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0")
+	/* test */
+	implementation("junit", "junit", "4.12")
+}
+
+
+// Maven configuration
+publishing {
+	/* repositories */
+	repositories {
+		/* maven publishing */
+		maven {
+			name = "GithubPackages"
+			url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+			version = project.version as String
+			// Authentication
+			credentials {
+				username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+				password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+			}
+		}
+	}
+}
---
+++ src/main/kotlin/com/github/ushiosan23/networkutils/http/HttpConnector.kt
@@ -0,0 +1,6 @@
+package com.github.ushiosan23.networkutils.http
+
+/**
+ * Http object connector
+ */
+object HttpConnector
