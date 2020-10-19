6948dddfe5c58ff484b3e523961692664cd9b1d0
Ushiosan23 <haloleyendee@outlook.com>
2020-10-19 17:14:19 -0500

    refactor(gradle): Change dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 	implementation(kotlin("reflect"))
 	/* serialization */
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
+	/* coroutines */
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0-M1")
 	/* test */
 	implementation("junit", "junit", "4.12")
 }
